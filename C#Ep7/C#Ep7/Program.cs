fbf92d5d0a0e1f7631c34069ec8a29e40f362bea
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-01-18 08:38:02 +0800

    Update Program.cs

--- C#Ep7/C#Ep7/Program.cs
+++ C#Ep7/C#Ep7/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C_Ep7
+namespace Sample
 {
     internal class Program
     {
@@ -100,6 +100,7 @@ namespace C_Ep7
             #endregion
 
             #region -- Nested For Loops --
+            /*
             Console.WriteLine("(Alcohol Eligibility Checker)");
             Console.Write("What is your age? ");
             int age = Convert.ToInt32(Console.ReadLine());
@@ -134,22 +135,106 @@ namespace C_Ep7
             {
                 Console.WriteLine("Out of Range Age! Please enter an age between 1 and 99!");
             }
+            */
             #endregion
 
             #region -- Equals Method -- 
 
+            Console.Write("Enter Username: ");
+            string user = Console.ReadLine();
+
             Console.Write("Enter Password: ");
             string pass = Console.ReadLine();
 
-            if(pass == "admin123")
+  
+            if (user.Equals("Jajangmyeon",StringComparison.InvariantCultureIgnoreCase) && pass.Equals("admin123",StringComparison.InvariantCultureIgnoreCase))
             {
                 Console.WriteLine("Welcome User!");
             }
-            else
+            else 
             {
                 Console.WriteLine("Access Denied!");
             }
+            #endregion
 
+            #region -- Using || (OR) Method with NESTED LOOP --
+
+            bool present = true;
+
+            Console.Write("Did you attend the class last week? (Please respond with 'present,' 'absent,' 'late', or 'cutting'): ");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() == "absent")
+            {
+                Console.WriteLine("No grade will be assigned!");
+            }
+            else if (response.ToLower() == "present" || response.ToLower() == "late" || response.ToLower() == "cutting")
+            {
+                if (response.ToLower() == "late")
+                {
+                    Console.Write("At what time did you attend? (1.00 - 4.00(class) if 4.01(its late): ");
+                    float time = Convert.ToSingle(Console.ReadLine());
+
+                    if (time >= 4.01f && time < 5.00f)
+                    {
+                        Console.WriteLine("You arrived late to the class, but you will receive a grade");
+                    }
+                    else if (time >= 5.01f && time < 6.00f)
+                    {
+                        Console.WriteLine("Grade will be fashionably late!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input correct response, Thanks!");
+                    }
+                }
+                else if (response.ToLower() == "cutting")
+                {
+                    Console.WriteLine("Your grade will be cut!");
+                }
+                else
+                {
+                    Console.Write("At what time did you attend? (1.00 - 4.00(class): ");
+                    float time = Convert.ToSingle(Console.ReadLine());
+
+                    if (present || (time >= 1.00f && time < 3.59f))
+                    {
+                        Console.WriteLine("You will receive a grade!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input correct response, Thanks!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please input correct response, Thanks!");
+            }
+
+            #endregion
+
+
+            #region -- Using ! (NOT) Method with NESTED LOOP
+            /*
+            Console.Write("You have a Ticket? Please Type Yes/No... ");
+            bool ticket = Console.ReadLine().Equals("Yes", StringComparison.OrdinalIgnoreCase);
+            int age = 18;
+
+            if(!(age >= 18))
+            {
+                if (ticket)
+                {
+                    Console.WriteLine("You can't Pass!");
+                }
+                
+            }
+            else 
+            {
+                Console.WriteLine("You can Pass!");
+            }
+            #endregion
+            */
             #endregion
 
 
