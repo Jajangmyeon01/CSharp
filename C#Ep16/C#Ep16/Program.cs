6eebf1138cc61f9756437d2009d336a446ec338f
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-13 00:06:31 +0800

    Add files via upload

---
+++ C#Ep16/C#Ep16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Constructors --
+            Console.WriteLine("(Constructors)");
+
+            /*
+            Product p1 = new Product("Cherry",189.4f);
+            Product p2 = new Product("Avocado",124.6f);
+            Product p3 = new Product("Orange",80.0f);
+            */
+
+            Student s1 = new Student("Gen Exus","Vergara", 3, 'B');
+
+            Student s2 = new Student("Anna", "Kendrick", 4, 'A');
+
+            Student s3 = new Student("Justine", "Timberlake", 2 , 'C');
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Ep16/C#Ep16/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep16
+{
+    internal class Student
+    {
+        public string firstName;
+        public string lastName;
+        public int grade;
+        public char section;
+
+        public Student(string firstName, string lastName, int grade, char section)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.grade = grade;
+            this.section = section;
+
+            Console.WriteLine();
+            Console.WriteLine("Student Created!\n");
+            Console.WriteLine($"Full Name: {firstName + " " + lastName + "\nGrade : " + grade +  "\nSection : " + section}");
+           
+        }
+    }
+}
