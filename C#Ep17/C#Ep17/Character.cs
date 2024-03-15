6c8c8b9239846d5e2b6155eab20466b7a5ee3ba7
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-15 13:25:13 +0800

    Add files via upload

---
+++ C#Ep17/C#Ep17/Character.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep17
+{
+    internal class Character
+    {
+        public string name, dialog, stats;
+        public int hp, mp, lvl;
+
+        #region -- Constructor --
+        // -- Constructor --
+        public Character(string name, string dialog, int hp, int mp, int lvl, string stats)
+        {
+            this.name = name;
+            this.dialog = dialog;
+            this.hp = hp;
+            this.mp = mp;
+            this.lvl = lvl;
+        }
+        #endregion
+
+
+        #region -- Methods -- 
+        // -- Methods --
+        public void introduce()
+        {
+            Console.WriteLine($"Hi my name is : {name}");
+        }
+        public void sayDialog()
+        {
+            Console.WriteLine($"{name} : {dialog} ");
+        }
+        public void health()
+        {
+            Console.WriteLine($"Health : {hp}");
+        }
+        public void manaPower()
+        {
+            Console.WriteLine($"Mana Power : {mp}");
+        }
+        public void level()
+        {
+            Console.WriteLine($"Level : {lvl}");
+        }
+        #endregion
+
+        #region -- Using return type --
+        /* -- Using return type --
+        public string checkStats()
+        {
+            return $"{stats}";
+     
+        }
+        */
+        #endregion
+
+
+
+    }
+}
---
+++ C#Ep17/C#Ep17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Object Methods --
+            Console.WriteLine("(Object Methods)");
+
+            #region -- Instantiation --
+            // -- Instantiaon --
+            Character c1 = new Character("Gen", "Always be Happy!", 100, 50, 20, "Happy!");
+            c1.introduce();
+            c1.sayDialog();
+            c1.health();
+            c1.manaPower();
+            c1.level();
+            Character c2 = new Character("Anna", "Always be Sad!", 50, 80, 100, "Sad");
+            #endregion
+
+            /* -- Calling return type in Main --
+            Console.WriteLine(c1.checkStats());
+            */
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
