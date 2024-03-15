06916b2c3d4c2c9fcfef242c077b372888177b8e
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-15 13:32:23 +0800

    Add files via upload

---
+++ C#Ep18/C#Ep18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Encapsulation --
+            /*
+            Console.WriteLine("(Encapsulation)");
+
+            User p = new User(999, "Gen", "Vergara", 21);
+
+            Console.WriteLine(p.FullName);
+
+
+         
+            p.UserID = 1001;
+            Console.WriteLine(p.UserID);
+            */
+
+            #endregion
+
+            User student = new User("Gen", "genexus01", "genexusvergara01@gmail.com", 1940);
+      
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ C#Ep18/C#Ep18/User.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace C_Ep18
+{
+    internal class User
+    {
+
+        #region -- Private Attributes / Fields 
+        /*
+        private int userID;
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        #endregion
+
+        #region -- Properties --
+        public int UserID
+        {
+            get { return userID; }
+            set {
+                Console.Write("Enter User Id : ");
+                Console.ReadLine();
+                if (value >= 1000) userID = value;
+                else Console.WriteLine("Error it must not below 1000");               
+            }
+        }
+        public string FirstName 
+        {
+            get { return firstName; }
+            set { firstName = value; }  
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string FullName
+        {
+            get { return $"FullName : {firstName + " " + LastName}";}
+        }
+        */
+
+
+        #region -- Constructor --
+        /*
+        public User (int userId, string firstName, string lastName, int age)
+        {  
+            UserID = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        */
+        #endregion
+
+        #endregion
+
+        #region -- Automatic Properties --
+
+        public string studentName { get; set; }
+        public string passWord { get; set; }       
+        public string email { get; set; }
+        public int postalCode { get; set; }
+
+
+        public User(string studentName, string passWord, string email, int talCode)
+        {
+            this.studentName = studentName;
+            this.passWord = passWord;
+            this.email = email;
+            this.postalCode = talCode;
+        }
+        #endregion
+
+    }
+}
