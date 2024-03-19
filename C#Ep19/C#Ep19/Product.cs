fffc87196a62e7359015a127ba4bce939efbc965
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-03-19 23:56:18 +0800

    Add files via upload

---
+++ C#Ep19/C#Ep19/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep19
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int stock { get; set; }
+        public float price { get; set; }
+
+        public Product(int ID, string Name, string Description, int stock, float price)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Description = Description;
+            this.stock = stock;
+            this.price = price;
+        }
+        // -- This is Overload Constructor does not have a description -- //
+        public Product(int ID, string Name, int stock, float price)
+        {
+            this.ID=ID;
+            this.Name = Name;
+            this.stock=stock;
+            this.price = price;
+        }
+        public Product(string Name, string Description, int stock, float price)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.stock = stock;
+            this.price = price;
+        }
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(ID.ToString()))
+                // -- This will print Product 3 -- //
+                return $"Name: {Name}\nPrice: {price}\nStock : {stock}\nPrice : {price}";
+         
+            else if (string.IsNullOrEmpty(Description))
+                // -- This will print the Product2 -- //
+                return $"ID: {ID}\nName: {Name}\nPrice: {price}\nStock : {stock}\nPrice : {price}";
+            else
+                // -- This will print the Product1 -- //
+                return $"ID: {ID}\nName : {Name}\nDescription : {Description}\nStock: {stock}\nPrice: {price}";
+   
+            }
+        }
+}
---
+++ C#Ep19/C#Ep19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ep19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -- Overloading Constructors --
+            Console.WriteLine("(Overloading Constructors)");
+
+            Product Prod1 = new Product(1, "Banana", "Banana is a tropical fruit that has a thick yellow or red skin and a soft edible flesh.", 10, 62.62f);
+            Product Prod2 = new Product(2, "Mango", 20, 87.40f);
+            Product Prod3 = new Product("Apple", "An apple is a fruit of the domesticated tree Malus domestica, which belongs to the rose family.", 15, 50.12f);
+
+            Console.WriteLine(Prod1);
+            Console.WriteLine();
+            Console.WriteLine(Prod2);
+            Console.WriteLine();
+            Console.WriteLine(Prod3);
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
